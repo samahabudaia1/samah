b68b4e700ad0a2ea925c7aa9af3a1d035de11237
samahABf <samah.abudaia.1994@gmail.com>
2018-01-10 10:09:59 +0200

    6.13

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,11 +3,12 @@ class Comment < ApplicationRecord
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
+   after_create_commit {CommentUpdateJob.perform_later(self, self.user)}
+
  belongs_to :user
   belongs_to :product
   	scope :rating_desc, -> { order(rating: :desc) }
 scope :rating_asc, -> { order(rating: :asc) }
-
 private
 
   def comment_params
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-
+$redis = Redis.new(:host => 'localhost', :port => 6379)
 
 if Rails.env.production?
 	$redis = Redis.new(url: ENV["REDIS_URL"])
